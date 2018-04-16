69833ba220bfa03441fa36ce98f3b27cee7d806c
nottnt <tanatat19@outlook.com>
2018-04-17 01:20:14 +0700

    test get id success

--- server.js
+++ server.js
@@ -39,9 +39,38 @@ nightmare
     }
     return data
   })
+  nightmare
+  .goto('http://klogic.kmutnb.ac.th:8080/kris/tess/dataQuerySelector.jsp?query=teachTab')
+.wait(1000)
+.select('select[name="facCode"]', '06')
+.wait(1000)
+.select('select[name="lectCode"]', 'SLJ')
+.wait(1000)
+.evaluate(function () {
+  var data = []
+  var x = document.body.getElementsByTagName('table')[6].getElementsByTagName('tr')
+  for (var i = 1; i < x.length; i++) {
+    data.push(
+      {
+        id: x[i].getElementsByTagName('td')[0].innerText,
+        name: x[i].getElementsByTagName('td')[1].innerText
+      }
+    )
+  }
+  return data
+  
   .end()
-  .then(console.log)
-  .catch(error => {
-    console.error('Search failed:', error)
-  })
+.then((value) => {
+    
+  res.send({value})
 })
+.catch(error => {
+  console.error('Search failed:', error)
+})
+  // res.send({data})
+})
+
+
+
+
+
