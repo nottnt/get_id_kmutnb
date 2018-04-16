3d3c5ea659c9fd11fe255b11adc3879a5cd045b2
nottnt <tanatat19@outlook.com>
2018-04-16 22:00:57 +0700

    get id night

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 var app = require('express')();
 var port = process.env.PORT || 5000;
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
+
+// app.get('/', function (req, res) {
+//   res.send('<h1>Hello Node.js</h1>')
+// })
+// app.get('/:id', function (req, res) {
+//   let id = req.params.id
+  
+//   res.send({stdid : id})
+// })
+
+// app.listen(port, function() {
+//   console.log('Starting node.js on port ' + port)
+// })
+
+// console.log('hello fucking project!!!')
 
 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello Node.js</h1>')
-})
-app.get('/fuck/:id', function (req, res) {
-  let id = req.params.id
-  res.send({stdid : id})
-})
-
-app.listen(port, function() {
-  console.log('Starting node.js on port ' + port)
-})
-
-console.log('hello fucking project!!!')
+nightmare
+  .goto('http://klogic.kmutnb.ac.th:8080/kris/tess/dataQuerySelector.jsp?query=teachTab')
+  .wait(1000)
+  .select('select[name="facCode"]', '06')
+  .wait(1000)
+  .select('select[name="lectCode"]', 'AMK')
+  .wait(1000)
+  .evaluate(function () {
+    var data = []
+    var x = document.body.getElementsByTagName('table')[6].getElementsByTagName('tr')
+    for (var i = 1; i < x.length; i++) {
+      data.push(
+        {
+          id: x[i].getElementsByTagName('td')[0].innerText,
+          name: x[i].getElementsByTagName('td')[1].innerText
+        }
+      )
+    }
+    return data
+  })
+  .end()
+  .then(console.log)
+  .catch(error => {
+    console.error('Search failed:', error)
+  })
