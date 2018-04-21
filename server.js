c17a5a50835890d207554f03d36597110475f81b
nottnt <tanatat19@outlook.com>
2018-04-21 20:43:11 +0700

    kuy

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require('express')();
-var port = process.env.PORT || 5000;
+//var port = process.env.PORT || 5000;
 const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: false })
 
@@ -18,7 +18,7 @@ app.listen(port, function() {
 
 // console.log('hello fucking project!!!')
 
-app.get('/getid', function (req, res) {
+app.get('/', function (req, res) {
 nightmare
   .goto('http://klogic.kmutnb.ac.th:8080/kris/tess/dataQuerySelector.jsp?query=teachTab')
   .wait(1000)
