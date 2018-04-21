68b14abd20a89fb5df717d3cc018f1fae3ea5b60
nottnt <tanatat19@outlook.com>
2018-04-21 19:29:03 +0700

    test api

--- server.js
+++ server.js
@@ -24,30 +24,84 @@ nightmare
   .wait(1000)
   .select('select[name="facCode"]', '06')
   .wait(1000)
-  .select('select[name="lectCode"]', 'AMK')
+  .select('select[name="lectCode"]', 'SLJ')
   .wait(1000)
   .evaluate(function () {
     var data = []
-    var x = document.body.getElementsByTagName('table')[6].getElementsByTagName('tr')
-    for (var i = 1; i < x.length; i++) {
-      data.push(
-        {
-          id: x[i].getElementsByTagName('td')[0].innerText,
-          name: x[i].getElementsByTagName('td')[1].innerText
-        }
-      )
+    var tablerow = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')
+    var tableday = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[0].getElementsByTagName('td')
+    var dayM = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[1]
+    var dayT = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[2]
+    var dayW = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[3]
+    var dayH = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[4]
+    var dayF = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[5].getElementsByTagName('td')
+
+
+
+// for(var c = 1;c<tablerow.length;c++){ข้อมูลตารางสอน
+
+// data.push(
+//   {
+//     datarow : tablerow[c].getElementsByTagName('td')[0].innerText,
+//     datarow2 : tablerow[c].getElementsByTagName('td')[1].innerText,
+//     count : tablerow.length
+
+//   }
+
+// )
+// }
+
+for (var i = 1; i < tableday.length; i++) {
+  data.push(
+    {
+      day: tableday[i].innerText,
+      // subject: tablerow[i].getElementsByTagName('td')[1].innerText,
+      // part: i,
+      count: tableday.length
+      //m:dayM.innerText,
+      // t:dayT.innerText,
+      // w:dayW.innerText,
+      // h:dayH.innerText,
+       //f:dayF.getElementsByTagName('td')[1].innerText
+
+
     }
+  )
+}
+    
+    // for (var i = 0; i < tableday.length; i++) {//วันเวลา
+    //     data.push(
+    //       {
+    //         day: tableday[i].innerText,
+    //         // subject: tablerow[i].getElementsByTagName('td')[1].innerText,
+    //         // part: i,
+    //         count: tableday.length
+    //         //m:dayM.innerText,
+    //         // t:dayT.innerText,
+    //         // w:dayW.innerText,
+    //         // h:dayH.innerText,
+    //          //f:dayF.getElementsByTagName('td')[1].innerText
+
+
+    //       }
+    //     )
+    //   }
+  
+    // return data
     return data
   })
 
 .end()
+
 .then((value) => {
     
-  res.send({value})
+  res.send(value)
 })
 .catch(error => {
-  console.error('Search failed:', error)
+ 
+  console.log(error)
 })
+
 })
 
 
