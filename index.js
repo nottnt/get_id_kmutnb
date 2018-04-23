2480cf3dff574cd5a1925894261c81669afcc210
nottnt <tanatat19@outlook.com>
2018-04-23 18:58:07 +0700

    gg

--- index.js
+++ index.js
@@ -1,70 +1,19 @@
-var app = require('express')();
-var port = process.env.PORT || 5000;
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: false })
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show:false });
 
+const URL = 'http://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/';
+console.log('Welcome to Nightmare scrape\n==========');
 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello Node.js</h1>')
-})
-// app.get('/:id', function (req, res) {
-//   let id = req.params.id
-  
-//   res.send({stdid : id})
-// })
-app.set('port',(process.env.PORT || 9000))
-app.listen(port, function() {
-  console.log('Starting node.js on port ' + port)
-})
-
-
-app.get('/getid', function (req, res) {
 nightmare
-  .goto('http://klogic.kmutnb.ac.th:8080/kris/tess/dataQuerySelector.jsp?query=teachTab')
-  .wait(1000)
-  .select('select[name="facCode"]', '06')
-  .wait(1000)
-  .select('select[name="lectCode"]', 'SLJ')
-  .wait(1000)
-  .evaluate(function () {
-    var data = []
-    var tablerow = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')
-    var tableday = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[0].getElementsByTagName('td')
-    var dayM = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[1]
-    var dayT = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[2]
-    var dayW = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[3]
-    var dayH = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[4]
-    var dayF = document.body.getElementsByTagName('table')[4].getElementsByTagName('tr')[5].getElementsByTagName('td')
-
-
-
-for(var c = 1;c<tablerow.length;c++){
-
-data.push(
-  {
-    datarow : tablerow[c].getElementsByTagName('td')[0].innerText,
-    datarow2 : tablerow[c].getElementsByTagName('td')[1].innerText,
-    count : tablerow.length
-
-  }
-
-)
-}
-
-    return data
-  })
-
-.end()
-
-.then((value) => {
-  console.log('test1'+value)
-  res.send({value})
-})
-.catch(error => {
- 
-  console.log(error)
-})
-
-})
-
-
+    .goto(URL)
+    .wait('.post-title')
+    .evaluate(() => document.querySelector('.post-title').textContent)
+    .end()
+    .then((result) => {
+        console.log(result);
+        console.log('=========\nAll done');
+    })
+    .catch((error) => {
+        console.error('an error has occurred: ' + error);
+    })
+    .then(() => (console.log('process exit'), process.exit()));
